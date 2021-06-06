f4f0e4ab3bf173adeae412ba93538dbd58b82a62
hihiroo <alsrud606@naver.com>
2021-06-06 17:35:13 +0900

    7. 드랍아웃 없애고 leakyReLU 0.01로, 에폭 30으로 변경. loss=0.018, val loss=0.028

--- VGG_MNIST/model/vgg16.py
+++ VGG_MNIST/model/vgg16.py
@@ -11,37 +11,37 @@ class VGG16(nn.Module):
         # 모델 구현
         self.conv = nn.Sequential(
             # vgg의 커널사이즈는 3x3으로 고정
-            nn.Conv2d(in_channels=_input_channel, out_channels=64, kernel_size=3, padding=1), nn.LeakyReLU(0.05), # 224x224x3 -> 224x224x64 / padding=1, stride=1, kernel=3이면 인풋 크기와 같음
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1), nn.LeakyReLU(0.05),
+            nn.Conv2d(in_channels=_input_channel, out_channels=64, kernel_size=3, padding=1), nn.LeakyReLU(0.01), # 224x224x3 -> 224x224x64 / padding=1, stride=1, kernel=3이면 인풋 크기와 같음
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1), nn.LeakyReLU(0.01),
             nn.MaxPool2d(kernel_size=2, stride=2), # 28x28x64 -> 14x14x64
 
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1), nn.LeakyReLU(0.05),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1), nn.LeakyReLU(0.05),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1), nn.LeakyReLU(0.01),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1), nn.LeakyReLU(0.01),
             nn.MaxPool2d(kernel_size=2, stride=2), # 14x14x128 -> 7x7x128
 
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),nn.LeakyReLU(0.05),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),nn.LeakyReLU(0.05),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),nn.LeakyReLU(0.05),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),nn.LeakyReLU(0.01),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),nn.LeakyReLU(0.01),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),nn.LeakyReLU(0.01),
             nn.MaxPool2d(kernel_size=2, stride=2, padding=1), # 7x7x256 -> 4x4x256
 
-            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1),nn.LeakyReLU(0.05),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),nn.LeakyReLU(0.05),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),nn.LeakyReLU(0.05),
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1),nn.LeakyReLU(0.01),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),nn.LeakyReLU(0.01),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),nn.LeakyReLU(0.01),
             nn.MaxPool2d(kernel_size=2, stride=2), # 4x4x512 -> 2x2x512
 
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),nn.LeakyReLU(0.05),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),nn.LeakyReLU(0.05),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),nn.LeakyReLU(0.05),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),nn.LeakyReLU(0.01),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),nn.LeakyReLU(0.01),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),nn.LeakyReLU(0.01),
             nn.MaxPool2d(kernel_size=2, stride=1), # 2x2x512 -> 1x1x512
         )
 
         self.fcLayer = nn.Sequential(
             nn.Linear(1*1*512,128), # 32*1x1x512를 fc하여 32x1x1x128으로
             nn.LeakyReLU(0.01),
-            nn.Dropout(p=0.5),
+            #nn.Dropout(p=0.5),
             nn.Linear(128, 128),
             nn.LeakyReLU(0.01),
-            nn.Dropout(p=0.5),
+            #nn.Dropout(p=0.5),
             nn.Linear(128, num_class),
             nn.Softmax(dim=1),
         )
